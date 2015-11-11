44504e2d30efca9c28f27d4f0b7960de18d760b2
manuelst <manuel.stettner@point-blank-international.com>
2015-11-11 15:29:53 +0100

    PBI Scripte Postflight Script

---
+++ PBI-Scripte Postflight.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+mv /Applications/PBI/com.pbi.startup.adhoc.plist ~/Library/LaunchAgents/
+mv /Applications/PBI/com.pbi.startup.meta.plist ~/Library/LaunchAgents/
+mv /Applications/PBI/com.pbi.startup.signatur.plist ~/Library/LaunchAgents/
+
+exit 0
