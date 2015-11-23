d1b6bb368c9482544a9b11123ec54c59ea79ba8f
Paul Galow <paul.galow@gmail.com>
2015-11-23 15:45:11 +0100

    Directory check and creation added

--- InstallStatus.sh
+++ InstallStatus.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 # Install status script. After that you can run it by simply typing "status" in Terminal
 
+# Get admin user password
+echo "Please enter your user name password:"
+sudo -v
+
+# Check for /usr/local/bin and install if not present
+sudo mkdir -p /usr/local /usr/local/bin
+
+# Assign rights for /usr/local/bin to local admin user
+chmod -R 755 /usr/local
+sudo chown -R $USER:$GROUP 755 /usr/local
+
+# Generate status script executable
 cat > /usr/local/bin/status << EOF
 #!/bin/bash
 bash <(curl -s https://raw.githubusercontent.com/pbihq/tools/master/Status.sh)
