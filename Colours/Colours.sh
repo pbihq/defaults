4c3940d0eaedc75a9dc397aab41009f4104addce
Paul Galow <paul.galow@gmail.com>
2015-12-01 12:07:27 +0100

    bugfix

--- Colours/Colours.sh
+++ Colours/Colours.sh
@@ -8,11 +8,14 @@
 if [[ ! -f ~/Library/Colors/NSColorPanelSwatches.plist ]]; then
 	cp /tmp/NSColorPanelSwatches.plist ~/Library/Colors
 	rm /tmp/NSColorPanelSwatches.plist
+	chown $USER:staff ~/Library/Colors/NSColorPanelSwatches.plist
 else
 	rm /tmp/NSColorPanelSwatches.plist
 fi
 
 # Install PBI Colours palette for current user
 cp /tmp/PBI\ Colours\ v1.1.clr ~/Library/Colors
+chown $USER:staff ~/Library/Colors/PBI\ Colours\ v1.1.clr
+rm /tmp/PBI\ Colours\ v1.1.clr
 
 exit 0
