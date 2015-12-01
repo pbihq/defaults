bc5bba2395c475660b2fac005f92b740bbe8ced4
Paul Galow <paul.galow@gmail.com>
2015-12-01 11:55:06 +0100

    Folder structure change

--- Colours/Colours.sh
+++ Colours/Colours.sh
@@ -2,17 +2,17 @@
 # 
 # Setup PBI Colours
 #
-# Note: PKG installer deploys PLIST and CLR payloads to $TMPDIR
+# Note: PKG installer deploys PLIST and CLR payloads to /tmp
 
 # Check for existing custom user colours and copy PLIST if they don't exist
 if [[ ! -f ~/Library/Colors/NSColorPanelSwatches.plist ]]; then
-	cp $TMPDIR/NSColorPanelSwatches.plist ~/Library/Colors
-	rm $TMPDIR/NSColorPanelSwatches.plist
+	cp /tmp/NSColorPanelSwatches.plist ~/Library/Colors
+	rm /tmp/NSColorPanelSwatches.plist
 else
-	rm $TMPDIR/NSColorPanelSwatches.plist
+	rm /tmp/NSColorPanelSwatches.plist
 fi
 
 # Install PBI Colours palette for current user
-cp $TMPDIR/PBI\ Colours\ v1.1.clr ~/Library/Colors
+cp /tmp/PBI\ Colours\ v1.1.clr ~/Library/Colors
 
 exit 0
