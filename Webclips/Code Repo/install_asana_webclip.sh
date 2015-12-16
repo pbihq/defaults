2df88cccf0fcb72ff0f299db6720613e8289e3c0
Paul Galow <paul.galow@gmail.com>
2015-12-16 16:19:32 +0100

    Webclips initial commit

---
+++ Webclips/Code Repo/install_asana_webclip.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Meraki Profil installieren
+/usr/bin/profiles -I -F /private/tmp/asana_webclip.mobileconfig
+
+# PBI Zertifikate von temporärer Location löschen
+rm -f /private/tmp/asana_webclip.mobileconfig
+
+logger "Asana Webclip installiert"
+exit 0
