472222fea3f73e2fd8e29e35cd4a35d05d0b61e1
manuelst <manuel.stettner@point-blank-international.com>
2016-06-03 16:36:13 +0200

    Email Notification added.

--- Signature/SignatureSetup.sh
+++ Signature/SignatureSetup.sh
@@ -9,10 +9,28 @@
 # Declare functions
 ###############################################################################
 
-# In the AccountsMaps.plist is a line like <string>imap://name.surname@berlin.point-blank-international.com/</string>. Find the line.
+# In the AccountsMaps.plist is a line like <string>imap://name.surname@mail.point-blank-international.com/</string>. Find the line.
 Mitarbeiterfile=~/Library/Mail/V3/MailData/Signatures/AccountsMap.plist
 Suchlinie=mail.point-blank-international.com
 Maillinie=$(grep "$Suchlinie" $Mitarbeiterfile)
+
+if [ -z "$MAName" ]; then
+	
+	echo "tell application \"Mail\"
+	
+	set theMessage to make new outgoing message with properties {subject:\"Signaturscript - Fehler\", content:\"Bei einem Mitarbeiter konnte der Name nicht ausgelesen werden.\", visible:false}
+	
+	tell theMessage
+		make new to recipient with properties {name:\"PBI Logs\", address:\"logs@point-blank-international.com\"}
+		
+		##Send the Message
+		send
+		
+	end tell
+end tell
+" | osascript
+	exit 1
+fi
 
 # Now extract only the name from this line. Sed takes the part starting with imap and ends with @. Then, in a second step, it deletes "imap://"
 MAName=$(echo $Maillinie | sed -e 's/^.*\(imap[^@]*\).*/\1/' -e 's/imap\:\/\///')
@@ -30,6 +48,24 @@ SignaturIDTemp=$(sed -e '1,'"$Signaturzeile"'d' -e "$SignaturzeileEnde"',1000d' 
 
 # Now get rid of the code around it.
 SignaturID=$(echo $SignaturIDTemp | sed -e 's/<string>//' -e 's/<\/string>//')
+
+if [ -z "$MAName" ]; then
+	
+	echo "tell application \"Mail\"
+	
+	set theMessage to make new outgoing message with properties {subject:\"Signaturscript - Fehler\", content:\"Bei $MAName konnte die Signatur-ID nicht ausgelesen werden.\", visible:false}
+	
+	tell theMessage
+		make new to recipient with properties {name:\"PBI Logs\", address:\"logs@point-blank-international.com\"}
+		
+		##Send the Message
+		send
+		
+	end tell
+end tell
+" | osascript
+	exit 1
+fi
 
 # Declare all the paths needed
 Source="https://cdn.pbi.online/signature/applemail/$MAName.mailsignature"
@@ -56,7 +92,19 @@ if [ -d ~/Library/Mail/V3/MailData/Signatures ]; then
 	chflags uchg "$DestinationFile"
 else
 	# In case of error give an error message.
-	echo "Die benötigten Ordner wurden nicht gefunden. Bitte informiere die PBI IT. $1" 1>&2
+	echo "tell application \"Mail\"
+	
+	set theMessage to make new outgoing message with properties {subject:\"Signaturscript - Fehler\", content:\"Bei $MAName wurde der Signatures-Ordner nicht gefunden oder die Dateien konnten nicht kopiert werden.\", visible:false}
+	
+	tell theMessage
+		make new to recipient with properties {name:\"PBI Logs\", address:\"logs@point-blank-international.com\"}
+		
+		##Send the Message
+		send
+		
+	end tell
+end tell
+" | osascript
 	exit 1
 fi
 
