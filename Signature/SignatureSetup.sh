91624534f4004b9fa61c79e446bc72ee60d816ff
Paul Galow <paul.galow@gmail.com>
2016-10-26 16:33:26 +0200

    Now supports macOS 10.12

--- Signature/SignatureSetup.sh
+++ Signature/SignatureSetup.sh
@@ -4,13 +4,23 @@
 # Signature Updater
 ###############################################################################
 # Prerequisites: Apple Mail Signature with name "PBI", assigned to PBI Mailadress
-# Works only with El Capitan!
+# Compatible with macOS 10.11 and 10.12
 ###############################################################################
 # Declare functions
 ###############################################################################
 
+# Get installed macOS version
+OperatingSystem=$(sw_vers -productVersion)
+
+# Set Mailversion for correct filepath
+case $OperatingSystem in
+10.11*) Mailversion="V3";;
+10.12*) Mailversion="V4";;
+*) echo "Error while setting mail version. Please note: This script only works with macOS 10.11 and 10.12"
+esac
+
 # In the AccountsMaps.plist is a line like <string>imap://name.surname@mail.point-blank-international.com/</string>. Find the line.
-Mitarbeiterfile=~/Library/Mail/V3/MailData/Signatures/AccountsMap.plist
+Mitarbeiterfile=~/Library/Mail/$Mailversion/MailData/Signatures/AccountsMap.plist
 Suchlinie=mail.point-blank-international.com
 Maillinie=$(grep "$Suchlinie" $Mitarbeiterfile)
 
@@ -27,17 +37,17 @@ fi
 
 # Send a mail if $MAName is empty
 if [ -z "$MAName" ]; then
-	
+
 	echo "tell application \"Mail\"
-	
+
 	set theMessage to make new outgoing message with properties {subject:\"Signaturscript - Fehler\", content:\"Bei einem Mitarbeiter konnte der Name nicht ausgelesen werden.\", visible:false}
-	
+
 	tell theMessage
 		make new to recipient with properties {name:\"PBI Logs\", address:\"logs@point-blank-international.com\"}
-		
+
 		##Send the Message
 		send
-		
+
 	end tell
 end tell
 " | osascript
@@ -45,7 +55,7 @@ end tell
 fi
 
 # In this file is a line <string>PBI</string>. Find it. But since the output will be something like "9: <string>PBI</string>", delete everything behind the line number (starting with ":")
-Signaturfile=~/Library/Mail/V3/MailData/Signatures/AllSignatures.plist
+Signaturfile=~/Library/Mail/$Mailversion/MailData/Signatures/AllSignatures.plist
 SignaturzeileTMP=$(grep -n 'PBI' $Signaturfile | cut -d":" -f1)
 
 # But since the line we want is two lines below, get that line instead.
@@ -60,17 +70,17 @@ SignaturID=$(echo $SignaturIDTemp | sed -e 's/<string>//' -e 's/<\/string>//')
 
 # Send a mail if $SignaturID is empty
 if [ -z "$SignaturID" ]; then
-	
+
 	echo "tell application \"Mail\"
-	
+
 	set theMessage to make new outgoing message with properties {subject:\"Signaturscript - Fehler\", content:\"Bei $MAName konnte die Signatur-ID nicht ausgelesen werden.\", visible:false}
-	
+
 	tell theMessage
 		make new to recipient with properties {name:\"PBI Logs\", address:\"logs@point-blank-international.com\"}
-		
+
 		##Send the Message
 		send
-		
+
 	end tell
 end tell
 " | osascript
@@ -79,8 +89,8 @@ fi
 
 # Declare all the paths needed
 Source="https://cdn.pbi.online/signature/applemail/$MAName.mailsignature"
-Destination=~/Library/Mail/V3/MailData/Signatures
-DestinationTemp=~/Library/Mail/V3/MailData
+Destination=~/Library/Mail/$Mailversion/MailData/Signatures
+DestinationTemp=~/Library/Mail/$Mailversion/MailData
 
 # Declare all the files needed
 TempFile=$(echo "$DestinationTemp/$SignaturID.mailsignature")
@@ -88,8 +98,8 @@ DestinationFile=$(echo "$Destination/$SignaturID.mailsignature")
 NameChange=$(echo "$DestinationTemp/$MAName.mailsignature")
 
 # A little check if the necessary folders exist. If not, abort
-if [ -d ~/Library/Mail/V3/MailData/Signatures ]; then
-	cd ~/Library/Mail/V3/MailData/Signatures
+if [ -d ~/Library/Mail/$Mailversion/MailData/Signatures ]; then
+	cd ~/Library/Mail/$Mailversion/MailData/Signatures
 	# Now magic happens. First, unlock the present mail signature.
 	chflags nouchg "$DestinationFile"
 	# Copy the file from Webspace to a temp folder
@@ -103,15 +113,15 @@ if [ -d ~/Library/Mail/V3/MailData/Signatures ]; then
 else
 	# In case of error give an error message.
 	echo "tell application \"Mail\"
-	
+
 	set theMessage to make new outgoing message with properties {subject:\"Signaturscript - Fehler\", content:\"Bei $MAName wurde der Signatures-Ordner nicht gefunden oder die Dateien konnten nicht kopiert werden.\", visible:false}
-	
+
 	tell theMessage
 		make new to recipient with properties {name:\"PBI Logs\", address:\"logs@point-blank-international.com\"}
-		
+
 		##Send the Message
 		send
-		
+
 	end tell
 end tell
 " | osascript
