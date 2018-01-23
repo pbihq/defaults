d055e71f5ff23cdc1a6b7a78143ca450b37398db
Paul Galow <paul.galow@gmail.com>
2018-01-23 11:28:07 +0100

    Cleaning up

--- PBI-Scripte Postflight.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-mv /Applications/PBI/com.pbi.startup.adhoc.plist ~/Library/LaunchAgents/
-mv /Applications/PBI/com.pbi.startup.meta.plist ~/Library/LaunchAgents/
-mv /Applications/PBI/com.pbi.startup.signatur.plist ~/Library/LaunchAgents/
-
-exit 0
